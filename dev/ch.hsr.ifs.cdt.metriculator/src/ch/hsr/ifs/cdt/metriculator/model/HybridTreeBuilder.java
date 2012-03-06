b62e7dcc8ec83156e5840d0f6d3788c3ea274caa
Jules Weder <julesweder@gmail.com>
2012-03-06 10:33:32 +0100

    refactoring - remove of Binding

--- dev/ch.hsr.ifs.cdt.metriculator/src/ch/hsr/ifs/cdt/metriculator/model/HybridTreeBuilder.java
+++ dev/ch.hsr.ifs.cdt.metriculator/src/ch/hsr/ifs/cdt/metriculator/model/HybridTreeBuilder.java
@@ -30,8 +30,6 @@ import ch.hsr.ifs.cdt.metriculator.model.nodes.WorkspaceNode;
 public class HybridTreeBuilder extends TreeBuilder {
 
 	private HashMap<String,AbstractNode> descendants                   = new HashMap<String,AbstractNode>();
-	private HashMap<IBinding, AbstractNode> declarations               = new HashMap<IBinding, AbstractNode>();
-	private HashMap<IBinding, AbstractNode> removedDeclarationBindings = new HashMap<IBinding, AbstractNode>();
 
 	//new merging
 	private HashMap<IBinding, AbstractNode> funcDeclarations = new HashMap<IBinding, AbstractNode>();
@@ -72,67 +70,6 @@ public class HybridTreeBuilder extends TreeBuilder {
 
 		return child;
 	}
-
-
-	private boolean sameNodeExists(AbstractNode child, AbstractNode existing) {
-		boolean sameNodeExists = false;
-		if(existing != null){
-			sameNodeExists = true;
-
-			if(child instanceof ILogicNode && existing.getNodeInfo().hasInfos()){
-				sameNodeExists = existing.getNodeInfo().equals(child.getNodeInfo());
-			}
-		}
-
-		if(foundForwardDeclarationAssociatedWith(child) && existing != null){
-			if((existing.getNodeInfo().isFunctionDeclarator() && child.getNodeInfo().isFunctionDefinition()) ||
-					existing.getNodeInfo().isElaboratedTypeSpecifier() && child.getNodeInfo().isCompositeTypeSpecifier()){
-				sameNodeExists = false;
-			}
-		}
-		return sameNodeExists;
-	}
-
-	private AbstractNode getDefinitionForDeclarationOf(AbstractNode child, AbstractNode parent) {
-		if(child.getNodeInfo().isElaboratedTypeSpecifier() || (child.getNodeInfo().isFunctionDeclarator() && !(parent.getNodeInfo().isFunctionDefinition()))){
-			IBinding binding = null;
-			if(child.getNodeInfo().isElaboratedTypeSpecifier()){
-				binding = child.getNodeInfo().getTypeBinding();
-			}else{
-				binding = child.getNodeInfo().getBinding();
-			}
-			if(removedDeclarationBindings.containsKey(binding)){
-				return removedDeclarationBindings.get(binding);
-			}else{
-				if(!child.getNodeInfo().isFriend()){
-					declarations.put(binding, child);
-				}
-			}
-		}
-		return null;
-	}
-
-	private boolean foundForwardDeclarationAssociatedWith(AbstractNode child) {
-		if(declarations.size() > 0){
-			IBinding binding = null;
-			if(child.getNodeInfo().isFunctionDefinition()){
-				binding = child.getNodeInfo().getBinding();
-			}
-			if(child.getNodeInfo().isCompositeTypeSpecifier()){
-				binding = child.getNodeInfo().getTypeBinding();
-			}
-			AbstractNode foundDecl = declarations.get(binding);
-			if(foundDecl != null){
-				declarations.remove(binding);
-				removedDeclarationBindings.put(binding, child);
-				foundDecl.removeFromParent();
-				foundDecl = null;
-				return true;
-			}
-		}
-		return false;
-	}
-
 
 	private void mergeChildrenOf(AbstractNode node, AbstractNode intoParent){
 		for(AbstractNode n : node.getChildren()){
