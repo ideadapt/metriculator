26feda70a05118bec6cc47565d1fdca21dd27166
ukunz <kunz@ideadapt.net>
2012-03-07 15:59:37 +0100

    supress compile error, see #205

--- dev/ch.hsr.ifs.cdt.metriculator.tagcloud/src/ch/hsr/ifs/cdt/metriculator/tagcloud/generateTagCloudAction.java
+++ dev/ch.hsr.ifs.cdt.metriculator.tagcloud/src/ch/hsr/ifs/cdt/metriculator/tagcloud/generateTagCloudAction.java
@@ -16,13 +16,17 @@ import ch.hsr.ifs.cdt.metriculator.tagcloud.model.Type;
 import ch.hsr.ifs.cdt.metriculator.tagcloud.views.TagCloudViewPart;
 import ch.hsr.ifs.cdt.metriculator.views.ITagCloudDataProvider;
 
-public class GenerateTagCloudAction implements org.eclipse.ui.IViewActionDelegate {
+/**
+ * TODO
+ * resolve bug #205
+ * */
+class GenerateTagCloudAction implements org.eclipse.ui.IViewActionDelegate {
 
 	private ITagCloudDataProvider view;
 	
 	@Override
 	public void run(IAction action) {
- 
+
 		if(view != null){
 			AbstractMetric metric = view.getMetric();
 			Collection<AbstractNode> nodes = view.getNodes();
