0711f4fabce7ce333b43cdf83ccfde56eab67ced
ukunz <kunz@ideadapt.net>
2012-08-06 19:53:30 +0200

    maven local repo in public path

--- dev/build.sh
+++ dev/build.sh
@@ -4,6 +4,6 @@ export PATH=/usr/local/apache-maven-3.0.4/bin:$PATH
 export DISPLAY=$DISPLAY
 THIS=$(readlink -f $0)
 BUNDLE_ROOT="`dirname $THIS`"
-
+echo $HOME
 cd $BUNDLE_ROOT
 mvn -e -Dmaven.repo.local=/var/m2 clean install
