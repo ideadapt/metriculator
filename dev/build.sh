10d30338858c1a2343881f9fefb2e1d3b526efb2
ukunz <kunz@ideadapt.net>
2012-08-06 19:40:36 +0200

    build.sh with new path

--- dev/build.sh
+++ dev/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PATH=/usr/local/apache-maven/apache-maven-3.0.3/bin:$PATH
+export PATH=/usr/local/apache-maven-3.0.4/bin:$PATH
 export DISPLAY=$DISPLAY
 THIS=$(readlink -f $0)
 BUNDLE_ROOT="`dirname $THIS`"
