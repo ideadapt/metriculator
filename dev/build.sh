f04dd4d118095cf84f99872677ba00b249d6ab12
ukunz <kunz@ideadapt.net>
2012-08-06 20:13:08 +0200

    no more display export in build script, use jenkins xvnc plugin instead

--- dev/build.sh
+++ dev/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 export PATH=/usr/local/apache-maven-3.0.4/bin:$PATH
-export DISPLAY=$DISPLAY
+#export DISPLAY=$DISPLAY
 THIS=$(readlink -f $0)
 BUNDLE_ROOT="`dirname $THIS`"
 echo $HOME
 cd $BUNDLE_ROOT
-mvn -e -Dmaven.repo.local=/var/m2 clean install
+mvn -e -Dmaven.repo.local=$HOME/.m2/repository clean install
