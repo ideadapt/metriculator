df44ef12fd625abf94274d883a2da8656a63b2df
ukunz <kunz@ideadapt.net>
2012-08-07 19:43:37 +0200

    try without display export

--- dev/build.sh
+++ dev/build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 # when using xvnc plug-in in jenkins results in error (if owner of .vnc and .xauthority are builders (tomcat, bitnami) otherwise the 
-#invalid type or access error is shown): 
+# 'wrong type or access mode for /home/bitnami/.vnc' error is shown): 
 #xauth:  timeout in locking authority file /home/bitnami/.Xauthority
 #New 'ip-10-252-138-103:26 (root)' desktop is ip-10-252-138-103:26
 #Starting applications specified in /home/bitnami/.vnc/xstartup
 #Log file is /home/bitnami/.vnc/ip-10-252-138-103:26.log
 
-# when using manually installed vnc4server with the export DISPLAY=$DISPLAY command, junit tests crash at any point
+# when using manually installed vnc4server with the export DISPLAY=$DISPLAY command, junit tests crash at any point with:
+# testCreateTreeFromFileWithLeadingSeparator(ch.hsr.ifs.cdt.metriculator.model.test.TreeBuilderTest)  Time elapsed: 0.122 sec  <<< ERROR!
+# java.lang.NoClassDefFoundError: org/eclipse/ui/wizards/datatransfer/IImportStructureProvider
+# ... ... ... Caused by: org.eclipse.swt.SWTError: No more handles [gtk_init_check() failed] at org.eclipse.swt.SWT.error(SWT.java:4308)
+# surprisingly env shows DISPLAY= (empty)
 export PATH=/usr/local/apache-maven-3.0.4/bin:$PATH
-export DISPLAY=$DISPLAY
+#export DISPLAY=$DISPLAY
 THIS=$(readlink -f $0)
 BUNDLE_ROOT="`dirname $THIS`"
 env
